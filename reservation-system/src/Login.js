22ba33d0f06b962129965c34adc4635d22d86a7b
Ivana Miovcic <ivanamiovcic@gmail.com>
2024-11-08 16:51:40 -0500

    Initial commit for app and login page

---
+++ reservation-system/src/Login.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault(); // prevents page from refreshing when you hit submit.
+    // Put code to login here. 
+    console.log('Logging in with:', { email, password });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <h2>Login</h2>
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <br></br>
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br></br>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
