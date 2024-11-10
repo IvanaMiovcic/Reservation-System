7d795a922dd7fcbf9d661a07e08e548fbb49afca
saysah <sayf.sahaf@gmail.com>
2024-11-10 00:21:42 -0500

    merge sayf-ui-implementation with latest main branch

---
+++ reservation-system/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Logo from "./Logo";
+
+export default function Header() {
+  return (
+    <div className="flex flex-row justify-start pt-10 sm:pb-6">
+      <div className="flex flex-row justify-center items-center gap-2 font-poppins text-white text-xl">
+        <Logo />
+        <div>SmartQ</div>
+      </div>
+    </div>
+  );
+}
---
+++ reservation-system/src/components/Logo.jsx
@@ -0,0 +1,20 @@
+export default function Logo() {
+  return (
+    <svg
+      width="25"
+      height="26"
+      viewBox="0 0 25 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1"
+        y="1.5"
+        width="23"
+        height="23"
+        stroke="white"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
