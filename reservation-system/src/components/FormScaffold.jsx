041f93a6f5a80b85cc354d878de037763214d254
saysah <sayf.sahaf@gmail.com>
2024-11-10 23:26:55 -0500

    test additional features and validation

--- reservation-system/src/components/FormScaffold.jsx
+++ reservation-system/src/components/FormScaffold.jsx
@@ -9,26 +9,31 @@ import {
   FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "./ui/form";
 import { Input } from "./ui/input";
 
 const schema = Joi.object({
-  firstname: Joi.string().min(3).max(25).required(),
-  lastname: Joi.string().min(3).max(25).required(),
+  firstname: Joi.string().min(2).required(),
+  lastname: Joi.string().min(2),
   password: Joi.string()
-    .pattern(new RegExp("^[a-zA-Z0-9!@#$%&^*()-+=/?<>,.]{12,30}"))
+    .min(8)
+    .max(25)
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d!@#$%^&*()-+=,.;'"/?<>{}]{12,}$/,
+    )
     .required(),
   repeat_password: Joi.ref("password"),
   email: Joi.string()
     .email({ tlds: { allow: ["com", "net"] } })
     .required(),
-  phone: Joi.string().pattern(new RegExp("^d{3}-d{3}-d{4}$|^d{10}$")),
-}).with("firstname", "lastname");
+  phone: Joi.string().regex(/^\d{3}-\d{3}-\d{4}$|^\d{10}$/),
+});
 
 function FormScaffold() {
-  const onSubmit = (data) => console.log(data);
+  function onSubmit(data) {
+    console.log(data);
+  }
   const form = useForm({
     resolver: joiResolver(schema),
     defaultValues: {
@@ -42,9 +47,12 @@ function FormScaffold() {
   });
 
   return (
-    <div className="">
+    <div>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 font-poppins"
+        >
           <FormField
             control={form.control}
             name="firstname"
@@ -58,10 +66,98 @@ function FormScaffold() {
                   />
                 </FormControl>
                 <FormDescription>This is your first name.</FormDescription>
+                <FormMessage />
               </FormItem>
             )}
           />
-          <Button>Submit</Button>
+          <FormField
+            control={form.control}
+            name="lastname"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Last Name"
+                    {...field}
+                    {...form.register("lastname")}
+                  />
+                </FormControl>
+                <FormDescription>This is your first name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Email"
+                    {...field}
+                    {...form.register("email")}
+                  />
+                </FormControl>
+                <FormDescription>This is your email address</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Phone"
+                    {...field}
+                    {...form.register("phone")}
+                  />
+                </FormControl>
+                <FormDescription>This is your phone number</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    {...field}
+                    {...form.register("password")}
+                  />
+                </FormControl>
+                <FormDescription>This is your password</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="repeat_password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Repeat Password"
+                    {...field}
+                    {...form.register("repeat_password")}
+                  />
+                </FormControl>
+                <FormDescription>Re-enter your password</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
         </form>
       </Form>
     </div>
