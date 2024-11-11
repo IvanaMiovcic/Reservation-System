2d02b143bec88b2a6d2e8990f8769d94f9459113
saysah <sayf.sahaf@gmail.com>
2024-11-11 01:21:34 -0500

    finish form validation implement

--- reservation-system/src/components/FormScaffold.jsx
+++ reservation-system/src/components/FormScaffold.jsx
@@ -14,25 +14,50 @@ import {
 import { Input } from "./ui/input";
 
 const schema = Joi.object({
-  firstname: Joi.string().min(2).required(),
-  lastname: Joi.string().min(2),
+  firstname: Joi.string().min(2).required().messages({
+    "string.empty": "First Name is Required",
+    "string.min": "First Name should be at least 2 characters long",
+  }),
+  lastname: Joi.string().min(2).messages({
+    "string.empty": "Last Name is Required",
+    "string.min": "Last Name should be at least 2 characters long",
+  }),
   password: Joi.string()
     .min(8)
     .max(25)
     .regex(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d!@#$%^&*()-+=,.;'"/?<>{}]{12,}$/,
     )
-    .required(),
+    .required()
+    .messages({
+      "string.empty": "Password is Required",
+      "string.pattern.base":
+        "Password should be at least 12 characters long, and contain numbers, uppercase letters, lowercae letters, and special characters",
+      "string.min":
+        "Password should be at least 12 characters long, and contain numbers, uppercase letters, lowercae letters, and special characters",
+    }),
   repeat_password: Joi.ref("password"),
   email: Joi.string()
     .email({ tlds: { allow: ["com", "net"] } })
-    .required(),
-  phone: Joi.string().regex(/^\d{3}-\d{3}-\d{4}$|^\d{10}$/),
+    .required()
+    .messages({
+      "string.email": "Email should be in a valid format",
+      "string.empty": "Email is Required",
+    }),
+  phone: Joi.string()
+    .regex(/^\d{3}-\d{3}-\d{4}$|^\d{10}$/)
+    .optional()
+    .messages({
+      "string.empty": "",
+      "string.pattern.base":
+        "Phone number must match either XXXXXXXXXX or XXX-XXX-XXXX",
+    }),
 });
 
-function FormScaffold() {
+export default function FormScaffold() {
   function onSubmit(data) {
-    console.log(data);
+    data.preventDefault();
+    console.log(data.firstname);
   }
   const form = useForm({
     resolver: joiResolver(schema),
@@ -157,11 +182,9 @@ function FormScaffold() {
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <Button type="submit">Create Account</Button>
         </form>
       </Form>
     </div>
   );
 }
-
-export default FormScaffold;
