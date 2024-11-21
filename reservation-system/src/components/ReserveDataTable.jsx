b785638c63e4bcabbec49fa7e50c4928e6f46064
saysah <sayf.sahaf@gmail.com>
2024-11-21 17:41:52 -0500

    implement handle for empty reservation list

--- reservation-system/src/components/ReserveDataTable.jsx
+++ reservation-system/src/components/ReserveDataTable.jsx
@@ -227,71 +227,77 @@ export default function ReserveDataTable() {
       <div className="flex flex-col rounded-md gap-3 p-5 h-full border">
         <div className="flex flex-row space-x-3 h-[8%]">
           <div className="text-white text-xl">Reservations</div>
-          <Badge className="">{reservations.length}</Badge>
+          <Badge className="">{reservations ? reservations.length : 0}</Badge>
         </div>
         <div className="flex flex-col h-[92%]">
-          <ScrollArea>
-            <div>
-              <Table className="text-white">
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="">Name</TableHead>
-                    <TableHead className="w-[15%]">Table</TableHead>
-                    <TableHead className="w-[15%]">Time</TableHead>
-                    <TableHead className="">Priority</TableHead>
-                    <TableHead></TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {reservations.map((reservation) => (
-                    <TableRow key={reservation.id}>
-                      <TableCell>{reservation.name}</TableCell>
-                      <TableCell>{reservation.table}</TableCell>
-                      <TableCell>{reservation.time}</TableCell>
-                      <TableCell>
-                        {reservation.priority === 1 ? (
-                          <Badge variant="destructive">High</Badge>
-                        ) : (
-                          <Badge variant="secondary">Standard</Badge>
-                        )}
-                      </TableCell>
-                      <TableCell className="text-center">
-                        <DropdownMenu>
-                          <DropdownMenuTrigger asChild>
-                            <Button variant="ghost">
-                              <Ellipsis />
-                            </Button>
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent align="end">
-                            <DropdownMenuLabel>
-                              Options for {reservation.name}
-                            </DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem
-                              onClick={() => console.log(reservation.id)}
-                            >
-                              Notify Customer
-                            </DropdownMenuItem>
-                            <DropdownMenuItem
-                              onClick={() => console.log(reservation.id)}
-                            >
-                              View Reservation Details
-                            </DropdownMenuItem>
-                            <DropdownMenuItem
-                              className="text-destructive"
-                              onClick={() => console.log(reservation.id)}
-                            >
-                              Delete Reservation
-                            </DropdownMenuItem>
-                          </DropdownMenuContent>
-                        </DropdownMenu>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
+          {reservations === null ? (
+            <div className="text-zinc-600 text-xs font-mono m-auto">
+              No reservations yet.
             </div>
-          </ScrollArea>
+          ) : (
+            <ScrollArea>
+              <div>
+                <Table className="text-white">
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="">Name</TableHead>
+                      <TableHead className="w-[15%]">Table</TableHead>
+                      <TableHead className="w-[15%]">Time</TableHead>
+                      <TableHead className="">Priority</TableHead>
+                      <TableHead></TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {reservations.map((reservation) => (
+                      <TableRow key={reservation.id}>
+                        <TableCell>{reservation.name}</TableCell>
+                        <TableCell>{reservation.table}</TableCell>
+                        <TableCell>{reservation.time}</TableCell>
+                        <TableCell>
+                          {reservation.priority === 1 ? (
+                            <Badge variant="destructive">High</Badge>
+                          ) : (
+                            <Badge variant="secondary">Standard</Badge>
+                          )}
+                        </TableCell>
+                        <TableCell className="text-center">
+                          <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                              <Button variant="ghost">
+                                <Ellipsis />
+                              </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                              <DropdownMenuLabel>
+                                Options for {reservation.name}
+                              </DropdownMenuLabel>
+                              <DropdownMenuSeparator />
+                              <DropdownMenuItem
+                                onClick={() => console.log(reservation.id)}
+                              >
+                                Notify Customer
+                              </DropdownMenuItem>
+                              <DropdownMenuItem
+                                onClick={() => console.log(reservation.id)}
+                              >
+                                View Reservation Details
+                              </DropdownMenuItem>
+                              <DropdownMenuItem
+                                className="text-destructive"
+                                onClick={() => console.log(reservation.id)}
+                              >
+                                Delete Reservation
+                              </DropdownMenuItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </div>
+            </ScrollArea>
+          )}
         </div>
       </div>
     </div>
