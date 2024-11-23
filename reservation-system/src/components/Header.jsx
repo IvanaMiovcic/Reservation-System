75536551e9bdb94f82ce63e5518bf3371b936c34
saysah <sayf.sahaf@gmail.com>
2024-11-22 19:49:35 -0500

    install supabase, configure auth methods for sign-in, sign-up, and sign-out

--- reservation-system/src/components/Header.jsx
+++ reservation-system/src/components/Header.jsx
@@ -1,13 +1,99 @@
 import Logo from "./Logo";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
+import { createClient } from "@supabase/supabase-js";
+import React, { useEffect, useState } from "react";
+import { Loader2 } from "lucide-react";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPA_URL,
+  import.meta.env.VITE_SUPA_ANON,
+);
 
 export default function Header() {
+  const [userStatus, setUserStatus] = useState("loading");
+
+  useEffect(() => {
+    isLoggedIn();
+    const timer = setInterval(isLoggedIn(), 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   //function to determine if a user is logged in and currently has a valid sesssion
-  const isLoggedIn = () => {
-    //logic to determine if user has a valid session goes here
-    return true;
+  async function isLoggedIn() {
+    try {
+      const { data, error } = await supabase.auth.getSession();
+
+      if (error) {
+        console.log(error);
+        setUserStatus("logged-out");
+        return;
+      }
+
+      if (!data?.session?.user) {
+        setUserStatus("logged-out");
+        return;
+      }
+
+      const accountType = data.session.user.user_metadata.account_type;
+
+      switch (accountType) {
+        case "Manager":
+        case "Employee":
+          setUserStatus("management");
+          break;
+
+        case "Customer":
+          setUserStatus("customer");
+          break;
+
+        default:
+          setUserStatus("logged-out");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const buttonTypes = {
+    loading: (
+      <Button disabled>
+        <Loader2 />
+      </Button>
+    ),
+    "logged-out": (
+      <Link to="log-in">
+        <Button variant="secondary">Log In</Button>
+      </Link>
+    ),
+    management: (
+      <Link to="employee-home">
+        <Button variant="secondary">Employee Dashboard</Button>
+      </Link>
+    ),
+    customer: (
+      <Link to="customer-home">
+        <Button variant="secondary">Customer Dashboard</Button>
+      </Link>
+    ),
   };
+
+  async function signOut() {
+    try {
+      const { error } = await supabase.auth.signOut();
+      console.log("Logged out");
+
+      if (error) {
+        console.log(error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const headerButton = buttonTypes[userStatus];
 
   return (
     <div className="flex flex-row justify-between pt-10 sm:pt-6">
@@ -15,17 +101,12 @@ export default function Header() {
         <Logo />
         <div>SmartQ</div>
       </div>
-      <div className="font-poppins">
-        {isLoggedIn() ? (
-          <Link to="/employee-home">
-            <Button variant="secondary">Dashboard</Button>
-          </Link>
-        ) : (
-          <Link>
-            <Button variant="secondary">Log In</Button>
-          </Link>
-        )}
+
+      <div className="font-poppins" onClick={() => signOut()}>
+        <Button variant="destructive">Sign Out</Button>
       </div>
+
+      <div>{headerButton}</div>
     </div>
   );
 }
