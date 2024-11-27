40bdccea0aa9dd87a124f983c94e3ea9badf0282
saysah <sayf.sahaf@gmail.com>
2024-11-27 02:11:44 -0500

    implement floor-view component for employee-dashboard. works as intended

--- reservation-system/src/components/FloorPlanView.jsx
+++ reservation-system/src/components/FloorPlanView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   TwoSeaterViewHori,
   TwoSeaterViewVerti,
@@ -10,35 +10,138 @@ import {
 } from "./tableView/SixSeaterView";
 import { createClient } from "@supabase/supabase-js";
 import { Badge } from "./ui/badge";
+import LoadingPage from "./LoadingPage";
 
 const supabase = createClient(
   import.meta.env.VITE_SUPA_URL,
   import.meta.env.VITE_SUPA_ANON,
 );
 
-const floorplan_id = "7c93a5b8-735f-4399-a3cb-ccf6d73c8762";
-
-async function getFloor(floorplan_id) {
-  try {
-    const { data, error } = await supabase
-      .from("floorplan")
-      .select("name, configuration")
-      .eq("floorplan_id", floorplan_id);
-
-    if (error) {
-      console.log(error);
-    }
-
-    return data[0];
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
-}
-
-const floorplan = await getFloor(floorplan_id);
-
 export default function FloorPlanView() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
+  const [floorplans, setFloorplans] = useState(null);
+  const [floorplanData, setFloorplanData] = useState(null);
+
+  useEffect(() => {
+    let restaurantFloorPlanChannel;
+    let floorPlanDataChannel;
+    async function getData() {
+      try {
+        const { data: userInfo, error } = await supabase.auth.getUser();
+        if (error) {
+          console.error(error);
+        }
+        setUserData(userInfo);
+
+        const { data: restaurantInfo } = await supabase
+          .from("works_at")
+          .select("restaurant_id")
+          .eq("user_id", userInfo.user.id);
+
+        const { data: restaurantFloorplans } = await supabase
+          .from("uses_floorplan")
+          .select("*")
+          .eq("restaurant_id", restaurantInfo[0].restaurant_id);
+        setFloorplans(restaurantFloorplans);
+
+        const { data: floorplanInfo } = await supabase
+          .from("floorplan")
+          .select("*")
+          .in(
+            "floorplan_id",
+            restaurantFloorplans.map((item) => item.floorplan_id),
+          );
+        setFloorplanData(floorplanInfo);
+
+        restaurantFloorPlanChannel = supabase
+          .channel("restaurantFloorplansUpdates")
+          .on(
+            "postgres_changes",
+            {
+              event: "*",
+              schema: "public",
+              table: "uses_floorplan",
+              filter: `restaurant_id=in.(${restaurantInfo[0].restaurant_id})`,
+            },
+            (payload) => {
+              switch (payload.eventType) {
+                case "INSERT":
+                  setFloorplans((prev) => [...prev, payload.new]);
+
+                  break;
+                case "UPDATE":
+                  setFloorplans((prev) =>
+                    prev.map((item) =>
+                      item.floorplan_id === payload.new.floorplan_id
+                        ? payload.new
+                        : item,
+                    ),
+                  );
+
+                  break;
+                case "DELETE":
+                  setFloorplans((prev) =>
+                    prev.filter(
+                      (item) => item.floorplan_id !== payload.old.floorplan_id,
+                    ),
+                  );
+
+                  break;
+              }
+            },
+          )
+          .subscribe();
+
+        floorPlanDataChannel = supabase
+          .channel("floorplanDataUpdates")
+          .on(
+            "postgres_changes",
+            {
+              event: "*",
+              schema: "public",
+              table: "floorplan",
+              filter: `floorplan_id=in.(${restaurantFloorplans.map((item) => item.floorplan_id)})`,
+            },
+            (payload) => {
+              switch (payload.eventType) {
+                case "INSERT":
+                  setFloorplanData((prev) => [...prev, payload.new]);
+                  break;
+                case "UPDATE":
+                  setFloorplanData((prev) =>
+                    prev.map((item) =>
+                      item.floorplan_id === payload.new.floorplan_id
+                        ? payload.new
+                        : item,
+                    ),
+                  );
+                  break;
+                case "DELETE":
+                  setFloorplanData((prev) =>
+                    prev.filter(
+                      (item) => item.floorplan_id !== payload.old.floorplan_id,
+                    ),
+                  );
+                  break;
+              }
+            },
+          )
+          .subscribe();
+
+        return () => {
+          supabase.removeChannel(restaurantFloorPlanChannel);
+          supabase.removeChannel(floorPlanDataChannel);
+        };
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, []);
+
   const renderComponent = (data) => {
     switch (data.table_type) {
       case "1":
@@ -64,24 +167,37 @@ export default function FloorPlanView() {
   const columns = 6;
   const totalCells = rows * columns;
 
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
   return (
-    <div className="flex flex-col rounded-md gap-4 p-5 h-1/2 border">
-      {floorplan ? (
+    <div className="flex flex-col rounded-md gap-4 p-5 h-full border">
+      {floorplans.find((item) => item.is_active === true) ? (
         <div className="flex flex-row text-xl gap-3 h-[8%]">
           <div className="text-white">Floor Status</div>
-          <Badge>{floorplan.name}</Badge>
+          <Badge>
+            {
+              floorplanData.find(
+                (item) =>
+                  item.floorplan_id ===
+                  floorplans.find((item) => item.is_active === true)
+                    ?.floorplan_id,
+              )?.name
+            }
+          </Badge>
         </div>
       ) : (
         <div className="flex flex-row text-xl h-[8%]">
-          <div className="text-white">Floor Status</div>
+          <div className="text-white">No active floorplan</div>
         </div>
       )}
 
       <div className="flex flex-row h-[92%] space-x-4">
         <div className="flex flex-row w-full">
-          {floorplan === null ? (
+          {!floorplans.find((item) => item.is_active === true) ? (
             <div className="text-zinc-600 text-xs font-mono m-auto">
-              No floorplans.
+              No active floorplan.
             </div>
           ) : (
             <div className="grid grid-flow-row grid-cols-6 grid-rows-3 w-full">
@@ -100,8 +216,20 @@ export default function FloorPlanView() {
                 return (
                   <div key={cellId} id={cellId}>
                     <div className="flex justify-center items-center col-span-1 row-span-1 text-white w-full h-full bg-background">
-                      {floorplan?.configuration[cellId] ? (
-                        renderComponent(floorplan.configuration[cellId])
+                      {floorplanData.find(
+                        (item) =>
+                          item.floorplan_id ===
+                          floorplans.find((item) => item.is_active === true)
+                            ?.floorplan_id,
+                      )?.configuration[cellId] ? (
+                        renderComponent(
+                          floorplanData.find(
+                            (item) =>
+                              item.floorplan_id ===
+                              floorplans.find((item) => item.is_active === true)
+                                ?.floorplan_id,
+                          )?.configuration[cellId],
+                        )
                       ) : (
                         <div className="text-zinc-600 text-xs font-mono"></div>
                       )}
