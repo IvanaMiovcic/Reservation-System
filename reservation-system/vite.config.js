f4049814c808fc55ad78cf2f55bae141784a6415
emadur20 <m18rahman@torontomu.ca>
2024-11-28 13:27:52 +0100

    Login test

--- reservation-system/vite.config.js
+++ reservation-system/vite.config.js
@@ -9,4 +9,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+    css: true
+  }
 });
