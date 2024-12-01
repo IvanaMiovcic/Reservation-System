2022b8744a3575081cb13cab6b13c8e6d28bca93
saysah <sayf.sahaf@gmail.com>
2024-11-30 19:51:18 -0500

    remove unused routes and components

--- reservation-system/src/CreateFloorplan.jsx
+++ reservation-system/src/CreateFloorplan.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import BackHeader from "./components/BackHeader";
 import FloorPlanDnD from "./components/FloorPlanDnD";
 
-export default function ModifyFloorplan() {
+export default function CreateFloorplan() {
   return (
     <div className="dark bg-background font-poppins w-[100%] h-screen flex flex-col">
       <div className="flex flex-col h-[100%] gap-4 p-4">
